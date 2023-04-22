18cac9be23fc50bca156c296013c2010bd30aff4
Claudio Correa <clcorrea@udec.cl>
2023-04-21 22:53:40 -0400

    debug

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import math as math
 #n = 3 # numero de salidas
 #m = 5 # numero de entradas
 
-red = [5,7,4,3]
+red = [3,3,3]
+output_ex = [3,3,3]
 
 
 #omega= weigths.dot(input_) - bias # pesos* entradas - sesgos 
@@ -15,26 +16,33 @@ def sigmoide(t):
         s.append(pow(1+math.exp(-i), -1))
     return s
 
-class capa:
+
+def dsigmoide(t):
+    return sigmoide(t)*(np.ones(len(t))-sigmoide(t))
+    
+
+class layer:
     def __init__(self, n, input):
+        self.a_i = input
         self.input = input # entradas
         self.weights = np.random.rand(n,len(self.input)) # peso
         self.bias = np.random.rand(n) # sesgos 
     def dprint(self):
-        print(' input: ',self.input,'\n','weigths:','\n',self.weights,'\n','bias:',self.bias)
+        print('a_i:',self.a_i ,' input: ',self.input,'\n','weigths:','\n',self.weights,'\n','bias:',self.bias)
         return   
 
 class network:
-    def __init__(self, neuron_for_layer, input):
+    def __init__(self, neuron_for_layer, input,output_ex):
+        self.output_ex = np.array(output_ex)
         self.input = input
         self.neuron_for_layer = neuron_for_layer
         self.layers = []
         for t in range(0,len(self.neuron_for_layer)-1):
             #print(self.neuron_for_layer[t+1])
             if not t == 0: 
-                self.layers.append(capa(self.neuron_for_layer[t+1],self.layers[t-1].bias))
+                self.layers.append(layer(self.neuron_for_layer[t+1],self.layers[t-1].bias))
             else:
-                self.layers.append(capa(self.neuron_for_layer[t+1],self.input))
+                self.layers.append(layer(self.neuron_for_layer[t+1],self.input))
 
     def sprint(self):
         for i in range(0,len(self.layers)):
@@ -43,13 +51,31 @@ class network:
     def run(self):
         for i in range(0,len(self.layers)):
             if not i == 0 : 
-                self.layers[i].input = sigmoide(np.add(self.layers[i-1].weights.dot(self.layers[i-1].input),self.layers[i-1].bias))
+                self.layers[i].a_i = np.add(self.layers[i-1].weights.dot(self.layers[i-1].input),self.layers[i-1].bias)
+                self.layers[i].input =sigmoide(self.layers[i].a_i)
             else:
                 print('luke es bonito ')
+    def learn(self):
+        C=0
+        for i in reversed(range(0,len(self.layers))):
+            if not i == 0 :
+                C= 2*(self.layers[i].a_i-self.output_ex)
+                print('C es :',C,'------>',i)
+            else :
+                print('C:',np.multiply(np.multiply(self.layers[i+1].weights, dsigmoide(self.layers[i+1].a_i)),C),'------>',i)
+                #print('C es :',C)
+        # error: C = sum_j=0^nl (a_j^(l) - y_i)^2 ,  yi <- vector de valores esperados
+        # where a_j^(l) := sigmoide(z_j^(l)) ,  z_j := sum_i^(nl-1 w_ij)
+        return ''
+
+
+
 
 
 input_ = np.random.rand(5) # sesgos 
-network_1=network(red, input_)
+network_1=network(red, input_, output_ex)
 network_1.run()
 network_1.sprint()
+print('-----------Etapa-de-aprendizaje----------')
+network_1.learn()
 #print(omega) 
